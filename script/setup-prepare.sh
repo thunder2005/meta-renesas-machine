cbc21d84cb5f620f2646c7551f3e6aec67a26147
yusw <yushengwei@e-planet.cn>
2019-06-30 12:01:51 +0800

    fix script

--- script/setup-prepare.sh
+++ script/setup-prepare.sh
@@ -2,7 +2,7 @@
 
 export WORK=`pwd`
 export SCRIPT_DIR=$WORK/sources/meta-renesas-machine/script
-source $SCRIPT_DIR/apply_patch.sh
-source $SCRIPT_DIR/download_proprietary.sh
-source $SCRIPT_DIR/apply_proprietary.sh
-source $SCRIPT_DIR/init_localconf.sh
+$SCRIPT_DIR/apply_patch.sh
+$SCRIPT_DIR/download_proprietary.sh
+$SCRIPT_DIR/apply_proprietary.sh
+$SCRIPT_DIR/init_localconf.sh
