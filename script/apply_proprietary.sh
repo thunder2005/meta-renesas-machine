74497ddb6172684d07506cdedf752f35a67f8ec8
yusw <yushengwei@e-planet.cn>
2019-06-30 12:09:05 +0800

    fix script

--- script/apply_proprietary.sh
+++ script/apply_proprietary.sh
@@ -6,5 +6,5 @@ echo "Apply Multimedia and Graphics library"
 export WORK=`pwd`
 export PKGS_DIR=$WORK/proprietary
 cd $WORK/sources/meta-renesas-machine
-sh copyscript/copy_evaproprietary_softwares.sh -f $PKGS_DIR
+sh machine/copyscript/copy_evaproprietary_softwares.sh -f $PKGS_DIR
 unset PKGS_DIR
--- script/setup-prepare.sh
+++ script/setup-prepare.sh
@@ -2,7 +2,7 @@
 
 export WORK=`pwd`
 export SCRIPT_DIR=$WORK/sources/meta-renesas-machine/script
-$SCRIPT_DIR/apply_patch.sh
+#$SCRIPT_DIR/apply_patch.sh
 $SCRIPT_DIR/download_proprietary.sh
 $SCRIPT_DIR/apply_proprietary.sh
 source $SCRIPT_DIR/init_localconf.sh
